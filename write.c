7dab9fd6c2ec4d8a3a98828a9ca120ec0ff6a790
Denis Kai Wilhelm <denis.kai.wilhelm@gmail.com>
2024-03-12 16:33:33 +0100

    update write.c to take 2 inputs

---
+++ write.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main (int argc, char *argv[]) {
+	int fd = open(argv[1], O_WRONLY|O_TRUNC|O_CREAT, S_IRUSR|S_IWUSR); //O_TRUNC sets file to 0
+	if (fd == -1) { perror("open"); return -1; }
+	char *msg = argv[2];
+	int len = strlen(msg);
+	int w = write(fd, msg, len);
+	if (w == -1) { perror("write"); return -1; }
+
+	return 0;
+}
