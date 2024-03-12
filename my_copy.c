c0fd66ad0c9af6788aff1a4832c2ad7067eda7da
Denis Kai Wilhelm <denis.kai.wilhelm@gmail.com>
2024-03-12 17:11:17 +0100

    implement copy

--- my_copy.c
+++ my_copy.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-// TODO ...
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-   // TODO ...
+	int fd = open(argv[1], O_RDONLY);
+
+	int len = 10;
+	char buf[len];
+	int r = read(fd, buf, len);
+
+
+	int fd_w = open(argv[2], O_WRONLY|O_TRUNC|O_CREAT, S_IRUSR|S_IWUSR);
+	int w;
+	while (r > 0) {
+		write(fd_w, buf, r);
+		r = read(fd, buf, len);
+	}
+
+	return 0;
 }
